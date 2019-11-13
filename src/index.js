9dbf24f2bc8be2d60fbe16a1279a7792e89d659b
Ashik Liyakathali <ashik21395@gmail.com>
2019-11-13 15:07:00 -0500

    modified index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './style/main.less';
 
 class Welcome extends React.Component {
-    render () {
-        return <h1>Hello World from React boilerplate</h1>;
-    }
+  render() {
+    return (
+      <body>
+        <h1 className="header">Hello World from React boilerplate</h1>
+        <p>
+          Visit my repository at{' '}
+          <a href="https://github.com/nsebhastian/my-react-boilerplate">
+            GitHub
+          </a>
+        </p>
+      </body>
+    );
+  }
 }
 ReactDOM.render(<Welcome />, document.getElementById('root'));
