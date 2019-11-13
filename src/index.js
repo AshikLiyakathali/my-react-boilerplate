794622ae5f1589819692e624ffa00365f471e353
Ashik Liyakathali <ashik21395@gmail.com>
2019-11-13 15:11:59 -0500

    modified index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,7 @@ import './style/main.less';
 
 class Welcome extends React.Component {
   render() {
-    return (
-      <body>
-        <h1 className="header">Hello World from React boilerplate</h1>
-        <p>
-          Visit my repository at{' '}
-          <a href="https://github.com/nsebhastian/my-react-boilerplate">
-            GitHub
-          </a>
-        </p>
-      </body>
-    );
+    return <h1>Hello World from React boilerplate</h1>;
   }
 }
 ReactDOM.render(<Welcome />, document.getElementById('root'));
